2af113b94eec2caa5d6985b3530aa74e3e564a39
HASANCoU <mehedi.hasancou2@gmail.com>
2026-05-05 11:21:44 +0600

    Fetch Category

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -1,9 +1,40 @@
 import Image from "next/image";
 
-export default function Home() {
+
+const getCategories = async()=>{
+    const res = await fetch('https://openapi.programming-hero.com/api/news/categories');
+    const data = await res.json();
+    return data.data;
+    
+}
+
+
+export default async function Home() {
+  const categories = await getCategories()
+  console.log("All Categories, ",categories.news_category);
   return (
-    <div className="w-11/12 mx-auto">
-      <h2 className="text-4xl text-red-500 text-center">The Home Page</h2>
+    <div className="container mx-auto grid grid-cols-12 gap-4 my-12">
+
+      <div className="col-span-3">
+        <h2 className="text-xl font-semibold text-gray-600">All Categories</h2>
+        <ul className="space-y-2 mt-4">
+          {
+            categories.news_category.map(category=><li key={category.category_name} className="bg-slate-200 p-2 rounded-md text-lg">{category.category_name}</li>)
+          }
+        </ul>
+      </div>
+
+
+
+      <div className="col-span-6 bg-purple-300">
+        <h2>All News</h2>
+      </div>
+
+
+      <div className="col-span-3 bg-red-300">
+        <h2>All Social</h2>
+      </div>
+
     </div>
   );
 }
